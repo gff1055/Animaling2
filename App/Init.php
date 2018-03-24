69c458b7dbbfe3c05682138f8736ac6fad8556a2
Guilherme Ferreira <gff105@gmail.com>
2018-03-24 13:05:37 -0300

    Iniciando o MVC na aplicacao...

--- App/Init.php
+++ App/Init.php
@@ -4,12 +4,26 @@ namespace App;
 
 class Init{
 
+	private $rotas;
+
 	function __construct(){
 		echo "Inicializando<br>";
 	}
 
+	public function configuraRotas(){
+		
+	}
+
+	public function run(){
+
+	}
+
 	public function urlDigit(){
 		return parse_url($_SERVER["REQUEST_URI"],PHP_URL_PATH);
+	}
+
+	public function setRotas($pRotas){
+		$this->rotas = $pRotas;
 	}
 
 }
