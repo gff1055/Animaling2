9df135cdff81f0318f95070ff70c36d35720d359
Guilherme Ferreira <gff105@gmail.com>
2018-03-27 17:23:48 -0300

    Controller de busca

---
+++ App/Controllers/Busca.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers;
+
+class Busca{
+
+	public function donos(){
+		echo "Eu sou a busca por dono";
+	}
+
+	public function animais(){
+		echo "Eu sou a busca por animais";
+	}
+
+	public function status(){
+		echo "Eu sou a busca por status";
+	}
+
+
+}
+
+?>
--- App/Init.php
+++ App/Init.php
@@ -7,22 +7,48 @@ class Init{
 	private $rotas;
 
 	function __construct(){
-		echo "Inicializando<br>";
+		$this->inicializarRotas();
+		$urlAcessada=$this->urlDigit();
+		$this->run($urlAcessada);
 	}
 
-	public function configuraRotas(){
-		
+	public function inicializarRotas(){
+		$arrayRotasAux['buscaAnimais'] = array(
+			'route'=>'/animaling2/public/busca/animal',
+			'controller'=>'busca',
+			'action'=>'animais'
+		);
+		$arrayRotasAux['buscaDono'] = array(
+			'route'=>'/animaling2/public/busca/dono',
+			'controller'=>'busca',
+			'action'=>'donos'
+		);
+		$arrayRotasAux['buscaStatus'] = array(
+			'route'=>'/animaling2/public/busca/status',
+			'controller'=>'busca',
+			'action'=>'status'
+		);
+
+		$this->configurarRotas($arrayRotasAux);
+
 	}
 
-	public function run(){
-
+	public function run($url){
+		foreach($this->rotas as $rota){
+			if($rota['route'] == $url){
+				$classe = 'App\Controllers\\'.ucfirst($rota['controller']);
+				$action = $rota['action'];
+				$controller = new $classe;
+				$controller->$action();
+			}
+		}
 	}
 
 	public function urlDigit(){
 		return parse_url($_SERVER["REQUEST_URI"],PHP_URL_PATH);
 	}
 
-	public function setRotas($pRotas){
+	public function configurarRotas(array $pRotas){
 		$this->rotas = $pRotas;
 	}
 
