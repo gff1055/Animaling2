2bcb6a8aa84e5716e98a5ccab3bf1b5016d9c3ca
gff1055 <gff105@gmail.com>
2018-03-27 00:11:53 -0300

    primeiros passos com o mvc

--- public/index.php
+++ public/index.php
@@ -9,3 +9,37 @@ $init = new Init();
 echo $init->urlDigit();
 
 ?>
+
+<html>
+	<head></head>
+	<body>
+		<a href="testeinserirdono.php">teste inserir dono</a>
+		<br>
+		<br>
+		<a href="php/testebusca.php">teste</a>
+		<br>
+		<a href="php/cadastro_usuario.php">testecadastri</a>
+		<br>
+		<a href="php/testealterar.php">testealterar</a>
+		<br>
+		<a href="php/testeexcluir.php">testeaexcluir</a>
+		<br>
+		<a href="php/testeBuscaAnimais.php">teste busca de animais</a>
+		<br>
+		<a href="php/testeBuscaStatus.php">teste busca de status</a>
+		<br>
+		
+		<a href="php/testeanimal.php">teste inserir animal</a>
+		<br>
+		<a href="php/testestatus.php">teste inserir status</a>
+		<br>
+		<a href="php/testeAlteraStatus.php">teste alterar status</a>
+		<br>
+		<a href="php/testeAmizade.php">teste amizade</a>
+		<br>
+		<a href="php/testeListarSeguidores.php">teste listar seguidores</a>
+		<br>
+		<a href="php/testeExcluirSeguidor.php">teste excluir seguidores</a>
+
+	</body>
+<html>
